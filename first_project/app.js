837e8fc0b1bd8938805f4474b21fdaf4ca184308
Deepjyoti Borah <deepborah86@gmail.com>
2021-06-12 22:40:52 +0530

    Adding files

--- first_project/app.js
+++ first_project/app.js
@@ -1,1 +1,13 @@
-console.log("Your code gors here.");
+function combine(input1, input2, resultCpnversion) {
+    var result;
+    if (typeof input1 === "number" && typeof input2 === "number" || resultCpnversion === 'as-number') {
+        result = +input1 + +input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+console.log(combine(23, 9, 'as-number'));
+console.log(combine('565', '45', 'as-number'));
+console.log(combine("Deep", "Borah", 'as-text'));
