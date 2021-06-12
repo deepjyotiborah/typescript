feee5821d6f6f077c54b0e0ac2f4f682aef6ee95
Deepjyoti Borah <deepborah86@gmail.com>
2021-06-12 22:36:52 +0530

    Adding files

--- first_project/app.ts
+++ first_project/app.ts
@@ -1,1 +1,16 @@
-console.log("Your code gors here.")
+function combine(input1: number | String,
+                 input2: number | String, 
+                resultCpnversion: 'as-number'|'as-text') {
+    let result;
+     if (typeof input1 === "number" && typeof input2 ==="number" || resultCpnversion === 'as-number') {
+        result = +input1 + +input2;
+     } else {
+         result = input1.toString() + input2.toString()
+     }
+         
+     return result;
+}
+
+console.log(combine(23, 9, 'as-number'))
+console.log(combine('565', '45', 'as-number'))
+console.log(combine("Deep", "Borah", 'as-text'));
---
+++ first_project/coreTypes.ts
@@ -0,0 +1,40 @@
+    function add(n1: number, n2: number, showResult: boolean, outputString: String) {
+        let result = n1 + n2;
+        if (showResult) {
+            return outputString + result.toString();
+        } else {
+            return result;
+        }
+    }
+
+    let num1: number = 3;
+    const num2: number = 5;
+    const outputStr: string = "The result is ";
+    console.log(add(num1, num2, true, outputStr))
+
+    enum DEPARTMENT {
+        IT = 1, SCIENCE = "SC", COMMERCE = 2
+    }
+
+    const person: {
+        name: string;
+        age: number;
+        hobbies: string[];
+        role: [number, string]; //  its a tuple
+        department: DEPARTMENT
+    }
+    = {
+        name: "Deep",
+        age: 34,
+        hobbies: ["football", "swimming","cooking"],
+        role: [2, "admin"],
+        department: DEPARTMENT.SCIENCE
+    }
+    console.log("Name is " + person.name);
+
+    for(const hobby of person.hobbies) {
+        console.log("-> " + hobby.toUpperCase());
+    }
+    if(person.department === DEPARTMENT.SCIENCE) {
+        console.log("In science.");
+    }
